xGreat/desktop

allow usage of public link to shared folders as an account URL
currently sync does not work

you can login automatically unles a password is needed

in that case, you get a prompt

Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

src/gui/owncloudsetupwizard.cpp
@@ -33,6 +33,7 @@
 #include "clientproxy.h"
 #include "filesystem.h"
 #include "owncloudgui.h"
+#include "account.h"
 
 #include "creds/credentialsfactory.h"
 #include "creds/abstractcredentials.h"
@@ -275,7 +276,11 @@ void OwncloudSetupWizard::slotFoundServer(const QUrl &url, const QJsonObject &in
         qCInfo(lcWizard) << " was redirected to" << url.toString();
     }
 
-    slotDetermineAuthType();
+    if (_ocWizard->account()->isPublicShareLink()) {
+        _ocWizard->setAuthType(DetermineAuthTypeJob::Basic);
+    } else {
+        slotDetermineAuthType();
+    }
 }
 
 void OwncloudSetupWizard::slotNoServerFound(QNetworkReply *reply)
@@ -323,6 +328,19 @@ void OwncloudSetupWizard::slotConnectToOCUrl(const QString &url)
     qCInfo(lcWizard) << "Connect to url: " << url;
     AbstractCredentials *creds = _ocWizard->getCredentials();
     _ocWizard->account()->setCredentials(creds);
+
+    if (_ocWizard->account()->isPublicShareLink()) {
+        _ocWizard->account()->setDavUser(creds->user());
+        _ocWizard->account()->setDavDisplayName(creds->user());
+
+        _ocWizard->setField(QLatin1String("OCUrl"), url);
+        _ocWizard->appendToConfigurationLog(tr("Trying to connect to %1 at %2 …")
+                                                .arg(Theme::instance()->appNameGUI())
+                                                .arg(url));
+
+        testOwnCloudConnect();
+        return;
+    }
 
     const auto fetchUserNameJob = new JsonApiJob(_ocWizard->account()->sharedFromThis(), QStringLiteral("/ocs/v1.php/cloud/user"));
     connect(fetchUserNameJob, &JsonApiJob::jsonReceived, this, [this, url](const QJsonDocument &json, int statusCode) {
@@ -711,6 +729,9 @@ AccountState *OwncloudSetupWizard::applyAccountChanges()
     auto manager = AccountManager::instance();
 
     auto newState = manager->addAccount(newAccount);
+    if (newAccount->isPublicShareLink()) {
+        qCDebug(lcWizard()) << "seeting up public share link account";
+    }
     manager->save();
     return newState;
 }

offsoc/nextcloud-desktop

src/gui/owncloudsetupwizard.cpp
@@ -34,6 +34,7 @@
 #include "sslerrordialog.h"
 #include "wizard/owncloudwizard.h"
 #include "wizard/owncloudwizardcommon.h"
+#include "account.h"
 
 #include "creds/credentialsfactory.h"
 #include "creds/abstractcredentials.h"
@@ -290,7 +291,11 @@ void OwncloudSetupWizard::slotFoundServer(const QUrl &url, const QJsonObject &in
         qCInfo(lcWizard) << " was redirected to" << url.toString();
     }
 
-    slotDetermineAuthType();
+    if (_ocWizard->account()->isPublicShareLink()) {
+        _ocWizard->setAuthType(DetermineAuthTypeJob::Basic);
+    } else {
+        slotDetermineAuthType();
+    }
 }
 
 void OwncloudSetupWizard::slotNoServerFound(QNetworkReply *reply)
@@ -339,6 +344,19 @@ void OwncloudSetupWizard::slotConnectToOCUrl(const QString &url)
     AbstractCredentials *creds = _ocWizard->getCredentials();
     if (creds) {
         _ocWizard->account()->setCredentials(creds);
+    }
+
+    if (_ocWizard->account()->isPublicShareLink()) {
+        _ocWizard->account()->setDavUser(creds->user());
+        _ocWizard->account()->setDavDisplayName(creds->user());
+
+        _ocWizard->setField(QLatin1String("OCUrl"), url);
+        _ocWizard->appendToConfigurationLog(tr("Trying to connect to %1 at %2 …")
+                                                .arg(Theme::instance()->appNameGUI())
+                                                .arg(url));
+
+        testOwnCloudConnect();
+        return;
     }
 
     const auto fetchUserNameJob = new JsonApiJob(_ocWizard->account()->sharedFromThis(), QStringLiteral("/ocs/v1.php/cloud/user"));
@@ -731,6 +749,11 @@ AccountState *OwncloudSetupWizard::applyAccountChanges()
     auto manager = AccountManager::instance();
 
     auto newState = manager->addAccount(newAccount);
+
+    if (newAccount->isPublicShareLink()) {
+        qCInfo(lcWizard()) << "seeting up public share link account";
+    }
+
     manager->saveAccount(newAccount.data());
     return newState;
 }