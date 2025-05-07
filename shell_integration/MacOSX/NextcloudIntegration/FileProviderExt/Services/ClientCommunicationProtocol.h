10d66330a19af5391c69cbbecc0f63c876e3d6d5
Claudio Cambra <claudio.cambra@nextcloud.com>
2025-05-07 11:20:38 +0800

    feat(shell_integration/macOS/FileProviderExt): Add ClientCommunicationProtocol method to get/set trash deletion enablement
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- shell_integration/MacOSX/NextcloudIntegration/FileProviderExt/Services/ClientCommunicationProtocol.h
+++ shell_integration/MacOSX/NextcloudIntegration/FileProviderExt/Services/ClientCommunicationProtocol.h
@@ -19,6 +19,8 @@
 - (void)createDebugLogStringWithCompletionHandler:(void(^)(NSString *debugLogString, NSError *error))completionHandler;
 - (void)getFastEnumerationStateWithCompletionHandler:(void(^)(BOOL enabled, BOOL set))completionHandler;
 - (void)setFastEnumerationEnabled:(BOOL)enabled;
+- (void)getTrashDeletionEnabledStateWithCompletionHandler:(void(^)(BOOL enabled, BOOL set))completionHandler;
+- (void)setTrashDeletionEnabled:(BOOL)enabled;
 
 @end
 
